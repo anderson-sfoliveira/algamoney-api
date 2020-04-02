53e7fdbfe511ac7f836b4caedf0b3fd1f676e363
Anderson Oliveira <anderson@sfoliveira@gmail.com>
2020-04-02 20:11:15 -0300

    Criado Filter para o CORS

---
+++ src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -0,0 +1,48 @@
+package com.example.algamoney.api.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+	private String originPermitida = "http://localhost:8000"; // TODO: Configurar para diferentes ambientes
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		resp.setHeader("Access-Control-Allow-Origin", originPermitida);
+		resp.setHeader("Access-Control-Allow-Credentials", "true");
+		
+		if ("OPTIONS".equals(req.getMethod()) && originPermitida.equals(req.getHeader("Origin"))) {
+
+			// MESMO REMOVENDO AS OPÇÕES DE CADA MÉTODO O "CLIENT" AINDA CONSEGUE FAZER A REQUISIÇÃO.... TESTAR
+			// No chrome não demonstra a requisição OPTIONS
+			
+			resp.setHeader("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT, OPTIONS");
+			resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			resp.setHeader("Access-Control-Max-Age", "10"); // tempo do pre-flight para fazer a requisição OPTIONS novamente = 10 segundos
+			
+			resp.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, resp);
+		}
+	}
+
+}
