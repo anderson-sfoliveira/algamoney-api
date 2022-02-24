b4aeeb914deb86133dd4aa39a187ff041bab2db3
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2022-02-24 16:42:17 -0300

    Update LancamentoService.java

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -1,6 +1,8 @@
 package com.example.algamoney.api.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.algamoney.api.model.Lancamento;
@@ -26,6 +28,24 @@ public class LancamentoService {
 		}
 		
 		return lancamentoRepository.save(lancamento);
+	}
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscaLancamentoPeloCodigo(codigo);
+		
+		Pessoa pessoa = this.pessoaRepository.findById(lancamento.getPessoa().getCodigo()).orElseThrow(() -> new PessoaInexistenteOuInativaException());
+
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");		
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private Lancamento buscaLancamentoPeloCodigo(Long codigo) {
+		Lancamento lancamentoSalvo = this.lancamentoRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return lancamentoSalvo;
 	}
 
 }
